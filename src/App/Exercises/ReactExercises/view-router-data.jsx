d2458dffd32faf3a8a5504ba777b427583decbf2
corpydev <szymanskiszy@gmail.com>
2023-10-14 10:51:04 +0200

    feat/wcz5/basic hit the mole

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -4,11 +4,13 @@ import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { ReactEventsUseStateMetaData } from './ReactEventsUseState/router-data';
 import { ReactIfStatementsMetaData } from './ReactIfStatements/router-data';
 import { ReactIfStatementsMoreOrLessMetaData } from './ReactIfStatementsMoreOrLess/router-data';
+import { hitTheMoleMetaData } from './HitTheMole/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactEventsUseStateMetaData,
   ReactIfStatementsMetaData,
   ReactIfStatementsMoreOrLessMetaData,
+  hitTheMoleMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
